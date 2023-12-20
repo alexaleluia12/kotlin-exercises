f3b77099c7fac35fd65bc8754b7a8fa1142999c3
alexaleluia12 <alexdiasaleluia@gmail.com>
2023-12-20 17:31:33 -0300

    implementação do donuts

--- app/src/main/kotlin/com/alexaleluia12/basic/string1.kt
+++ app/src/main/kotlin/com/alexaleluia12/basic/string1.kt
@@ -1,7 +1,28 @@
 package com.alexaleluia12.basic
+/*
+#!/usr/bin/python3
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+# Copyright 2023 Alex Aleluia
+# Licença MIT
+*/
+
+/*
+A. Dado um número inteiro de rosquinhas, retorne uma string
+no formato 'Number of donuts: <contagem>', onde <contagem> é o número
+passado. No entanto, se a contagem for 10 ou mais, use a palavra 'many'
+em vez da contagem real.
+Então donuts(5) retorna 'Number of donuts: 5'
+e donuts(23) retorna 'Number of donuts: many'
+*/
 fun donuts(n: Int): String {
-    return ""   
+    val count = if (n >= 10) "many" else n.toString()
+    return "Number of donuts: $count"
 }
 
 fun both_ends(s: String): String {
